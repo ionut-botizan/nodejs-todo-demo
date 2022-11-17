d6ebfe2889497a9eec675ce59f4e74f913293a08
Ionut Botizan <ionut.botizan@haufe-lexware.com>
2022-11-17 13:55:05 +0200

    Convert the view to React

--- client/App.jsx
+++ client/App.jsx
@@ -1,3 +1,37 @@
+import { useState } from 'react'
+import './styles.css'
+
 export default function App() {
-	return <h1>Hello from React</h1>
+	const [active, setActive] = useState([])
+	const [completed, setCompleted] = useState([])
+
+	return (
+		<div className="container">
+			<h2>A Simple ToDo List App</h2>
+
+			<form action="#" method="post">
+				<input type="text" name="title" placeholder="add new task" />
+				<button>Add Task</button>
+			</form>
+
+			<h2>Pending Tasks</h2>
+
+			<ul>
+				{active.map(task => (
+					<li key={task._id}>
+						<form action="#" method="post">
+							<input type="hidden" name="id" value={task._id} />
+							<button>✔</button> {task.title}
+						</form>
+					</li>
+				))}
+			</ul>
+
+			<h2>Completed Tasks</h2>
+
+			{completed.map(task => (
+				<li key={task._id}>✔ {task.title}</li>
+			))}
+		</div>
+	)
 }
